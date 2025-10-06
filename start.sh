7a917625a8a36e8c241609cfd1caa049459d95e8
Sarabsit <sarainam60@gmail.com>
2025-10-06 19:44:17 +0500

    Last Commit

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Backend folder me jao
+# Go to backend folder
 cd backend_lms
 
-# Python dependencies install karo
-pip install -r requirements.txt
+# Install dependencies
+pip3 install -r requirements.txt
 
-# Database migrations run karo
-python manage.py migrate
+# Run migrations
+python3 manage.py migrate
 
-# Static files collect karo (optional)
-python manage.py collectstatic --noinput
+# Collect static files
+python3 manage.py collectstatic --noinput
 
-# Django server start karo Railway port pe
-python manage.py runserver 0.0.0.0:$PORT
+# Run Django server
+python3 manage.py runserver 0.0.0.0:$PORT
