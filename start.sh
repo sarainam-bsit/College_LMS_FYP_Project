6f1bfb73e3f64e498ce1a8a23adfc7ea48f0556c
Sarabsit <sarainam60@gmail.com>
2025-10-06 20:12:12 +0500

    start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# Go to backend folder
+# Update package manager
+apt-get update -y
+
+# Install Python and pip
+apt-get install -y python3 python3-pip
+
+# Move to backend folder
 cd backend_lms
 
-# Install dependencies
+# Install requirements
 pip3 install -r requirements.txt
 
-# Run migrations
+# Apply migrations
 python3 manage.py migrate
 
 # Collect static files
 python3 manage.py collectstatic --noinput
 
-# Run Django server
-python3 manage.py runserver 0.0.0.0:$PORT
+# Run server
+python3 manage.py runserver 0.0.0.0:8000
